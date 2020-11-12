1df6648cd05c55db8c8866082691f3a5e65d4cb4
László Waldersee <waldersee2000@gmail.com>
2020-11-12 10:50:45 +0100

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ get "restaurant", to: "restaurant#index"
+ get "restaurant/new", to: "restaurant#new"
+ get "restaurant/:id", to: "restaurants#show"
+ post "restaurant", to: "restaurantss#create"
+ delete "restaurant/:id", to: "restaurant#destroy"
+ get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+puts "creating new restaurants"
+  5.times do
+   Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+ end
+ puts "Finished!"
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
